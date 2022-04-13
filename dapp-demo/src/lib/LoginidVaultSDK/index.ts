d7fa2f1a0c2b66ca3a8f2e8f7a017571a8352a1e
pd@loginid <pd@drawexpress.com>
2022-04-13 12:01:30 -0400

    add dapp-demo

---
+++ dapp-demo/src/lib/LoginidVaultSDK/index.ts
@@ -0,0 +1,107 @@
+
+interface SignTxnsError extends Error {
+    code: number;
+    data?: any;
+}
+
+export type TxnId = string;
+export type AlgorandAddress = string;
+
+// support enable function
+export interface EnableOpts  {
+    network?: string;
+    genesisID?: string;
+    genesisHash?: string;
+}
+
+export interface EnableResult {
+    genesisID: string;
+    genesisHash: string;
+    accounts: AlgorandAddress[];
+}
+
+export type SignTxnsOpts = {
+    /**
+     * Optional message explaining the reason of the group of transactions
+     */
+    message?: string;
+}
+
+export interface PostTxnsResult {
+    txnIds: TxnId[];
+    signTxn: string[];
+}
+
+export interface PostTxnsError extends Error {
+    code: number;
+    data?: any;
+    successTxnIds: (TxnId | null)[];
+} 
+
+
+export interface WalletTransaction {
+    /**
+     * Base64 encoding of the canonical msgpack encoding of a Transaction.
+     */
+    txn: string;
+    /**
+    * Optional authorized address used to sign the transaction when the account
+    * is rekeyed. Also called the signor/sgnr.
+    */
+   authAddr?: AlgorandAddress;
+
+   /**
+    * Optional list of addresses that must sign the transactions
+    */
+   signers?: AlgorandAddress[];
+
+   /**
+    * Optional base64 encoding of the canonical msgpack encoding of a 
+    * SignedTxn corresponding to txn, when signers=[]
+    */
+   stxn: string;
+
+   /**
+    * Optional message explaining the reason of the transaction
+    */
+   message?: string;
+
+   /**
+    * Optional message explaining the reason of this group of transaction
+    * Field only allowed in the first transaction of a group
+    */
+   groupMessage?: string;
+ }
+
+export class FidoVaultSDK {
+    /**
+    *  
+    *   https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0006.md
+    */
+
+    static async enable( network: EnableOpts): Promise<EnableResult | null> {
+        return Promise.resolve(null);
+    }
+
+    /**
+     * signTxns
+     * https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0001.md 
+     * https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0005.md 
+     * @returns {Promise<string|null> []}
+     * 
+    **/
+    static async signTxns(txns: WalletTransaction[], opts: SignTxnsOpts): Promise<(PostTxnsResult | null)[]> {
+        return Promise.resolve([]);
+    }
+
+
+    /**
+     * signAndPostTxns
+     * https://github.com/algorandfoundation/ARCs/blob/main/ARCs/arc-0008.md 
+     * @returns {Promise<string|null> []}
+     * 
+    **/
+    static async signAndPostTxns(txns: WalletTransaction[], opts: SignTxnsOpts): Promise<(PostTxnsResult | null)[]> {
+        return Promise.resolve([]);
+    }
+}
