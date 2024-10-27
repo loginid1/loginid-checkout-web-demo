c72dbd52e3b70c6326402ae621671d999a8fec1d
PD <peter@loginid.io>
2024-10-26 21:03:44 -0400

    update webview

--- passkey-only/src/pages/checkout/index.tsx
+++ passkey-only/src/pages/checkout/index.tsx
@@ -46,102 +46,112 @@ export interface CheckoutRequest {
     callback: string;
 }
 const mService = new MessagingService(window.parent);
-let payload : CheckoutRequest | null = null;
+let payload: CheckoutRequest | null = null;
 let redirect = false;
 export function CheckoutPage() {
     const [displayMessage, setDisplayMessage] = useState<DisplayMessage | null>(
-		null
-	);
+        null
+    );
     const [username, setUsername] = useState<string>("");
     const [token, setToken] = useState<string>("");
     const [view, setView] = useState<CheckoutViewEnum>(CheckoutViewEnum.Wait)
     const [searchParams, setSearchParams] = useSearchParams();
     const [regPasskey, setRegPasskey] = useState<boolean>(false);
-    
-    
-	useEffect(() => {
-        const query_data = searchParams.get("data")
-        if(query_data) {
+    const [webview, setWebview] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        const query_data = searchParams.get("data");
+        if (query_data) {
             payload = JSON.parse(base64UrlToString(query_data));
             setView(CheckoutViewEnum.Login);
             redirect = true;
         } else {
 
-		let target = window.parent;
-		if (target != null) {
-			mService.onMessage((msg, origin) => onMessageHandle(msg, origin));
-			//checkSession();
-		} else {
-			//setDisplayMessage({ text: "Missing dApp origin", type: "error" });
-			//navigate("/login");
-		}
+            let target = window.parent;
+            if (target != null) {
+                mService.onMessage((msg, origin) => onMessageHandle(msg, origin));
+                //checkSession();
+            } else {
+                //setDisplayMessage({ text: "Missing dApp origin", type: "error" });
+                //navigate("/login");
+            }
         }
-	}, []);
+        const query_webview = searchParams.get("webview");
+        if(query_webview) {
+            setWebview(true);
+        }
+
+    }, []);
 
 
-	const INTERVAL = 100;
-	const TIMEOUT = 10000;
-	async function waitForInput(): Promise<boolean> {
-		let wait = TIMEOUT;
-		while (wait > 0) {
-			if (payload == null) {
-				await new Promise((resolve) => setTimeout(resolve, INTERVAL));
-			} else {
-				return Promise.resolve(true);
-			}
-			wait = wait - INTERVAL;
-		}
-		return Promise.resolve(false);
-	}
+    const INTERVAL = 100;
+    const TIMEOUT = 10000;
+    async function waitForInput(): Promise<boolean> {
+        let wait = TIMEOUT;
+        while (wait > 0) {
+            if (payload == null) {
+                await new Promise((resolve) => setTimeout(resolve, INTERVAL));
+            } else {
+                return Promise.resolve(true);
+            }
+            wait = wait - INTERVAL;
+        }
+        return Promise.resolve(false);
+    }
 
-	function onMessageHandle(msg: Message, origin: string) {
-		try {
-			mService.origin = origin;
-			mService.id = msg.id;
-			let p: CheckoutRequest = JSON.parse(msg.data);
-			//console.log("request: " , p);
+    function onMessageHandle(msg: Message, origin: string) {
+        try {
+            mService.origin = origin;
+            mService.id = msg.id;
+            let p: CheckoutRequest = JSON.parse(msg.data);
+            //console.log("request: " , p);
             setUsername(p.preid);
-            if(p.preid != "") {
+            if (p.preid != "") {
                 setView(CheckoutViewEnum.Confirmation);
-            }  else {
+            } else {
                 setView(CheckoutViewEnum.Login);
-            } 
+            }
             payload = p;
-		} catch (error) {
-			console.log(error);
-		}
-	}
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     function renderView(view: CheckoutViewEnum) {
-            if(view == CheckoutViewEnum.Confirmation && payload != null) {
-                return <CheckoutConfirmPrompt username={username} token={token} request={payload} regPasskey={regPasskey} onComplete={onCheckoutConfirmHandle}/>;
+        if (view == CheckoutViewEnum.Confirmation && payload != null) {
+            return <CheckoutConfirmPrompt username={username} token={token} request={payload} regPasskey={regPasskey} onComplete={onCheckoutConfirmHandle} />;
 
-            } else if(view == CheckoutViewEnum.Wait) {
-                return <></>
-            } else {
-                return <CheckoutLoginPrompt onComplete={onCheckoutLoginHandle}/>;
-            }
+        } else if (view == CheckoutViewEnum.Wait) {
+            return <></>
+        } else {
+            return <CheckoutLoginPrompt onComplete={onCheckoutLoginHandle} />;
+        }
     }
 
     function onCheckoutLoginHandle(email: string, token: string, next: string) {
-        localStorage.setItem("preid-email",email);
+        localStorage.setItem("preid-email", email);
         setUsername(email);
         setToken(token);
         setView(CheckoutViewEnum.Confirmation);
-        if(next === "passkey") {
+        if (next === "passkey") {
             setRegPasskey(true);
         }
     }
 
     function onCheckoutConfirmHandle(email: string, token: string, next: string) {
-        localStorage.setItem("preid-email",email);
+        localStorage.setItem("preid-email", email);
         const baseCallback = payload?.callback || `http://localhost:3001/callback`;
-        const base64=stringToBase64Url(`{"email":"${email}","token":"${token}","passkey":"true"}`);
+        const base64 = stringToBase64Url(`{"email":"${email}","token":"${token}","passkey":"true"}`);
         const callback = baseCallback + `?data=${base64}`;
-        if(redirect) {
-            document.location.href = callback;
+        if (redirect) {
+            if(webview){
+                document.location.href = "abcbank://callback" + `?data=${base64}`;
+            } else {
+                document.location.href = callback;
+            }
         } else {
-            return mService.sendMessageData({email:email,  token:token, callback: baseCallback });
+            return mService.sendMessageData({ email: email, token: token, callback: baseCallback });
         }
     }
 
@@ -149,15 +159,15 @@ export function CheckoutPage() {
         <Center h="100vh" w="100%">
             <Card shadow="sm" w={{ base: "100%", md: 480, lg: 550 }} mih={420} p="sm">
                 <Flex justify="center" align="center" direction="column" w="100%">
-                {redirect && 
-                    <Image
-                        h={24}
-                        w={96}
-                        src="/assets/logo.svg"
-                        alt="LoginID Inc."
-                        mb="md"
-                    />
-                }
+                    {redirect &&
+                        <Image
+                            h={24}
+                            w={96}
+                            src="/assets/logo.svg"
+                            alt="LoginID Inc."
+                            mb="md"
+                        />
+                    }
                     {renderView(view)}
                 </Flex>
             </Card>
